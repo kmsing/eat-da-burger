427ba781e3fbc7055ff0531917e215909b55c7a6
Kristine Singleton <kmsing11@gmail.com>
2017-10-10 01:19:38 -0500

    adding file structure and sql inputs

---
+++ burger/db/seeds.sql
@@ -0,0 +1,6 @@
+USE "burgers_db";
+
+-- insert 3 burgers
+INSERT INTO "burgers_table" (burger_name, devoured, date) VALUES ('Hamburger', false, CURRENT_TIMESTAMP);
+INSERT INTO "burgers_table" (burger_name, devoured, date) VALUES ('BBQ', false, CURRENT_TIMESTAMP);
+INSERT INTO "burgers_table" (burger_name, devoured, date) VALUES ('Cheese', false, CURRENT_TIMESTAMP);
