3458f5d339919c74964fa977142627a3aee2b0e3
Kristine Singleton <kmsing11@gmail.com>
2017-10-18 22:22:33 -0500

    adding changes on all files

---
+++ burger/config/orm.js
@@ -0,0 +1,75 @@
+// import in the connection to the database
+var connection = require('../config/connection.js');
+
+function printQuestionMarks(num) {
+	var arr = [];
+
+	for (var i = 0; i < num; i++) {
+		arr.push('?');
+	}
+
+	return arr.toString();
+}
+
+
+function objToSql(ob) {
+	var arr = [];
+
+	for (var key in ob) {
+		if (ob.hasOwnProperty(key)) {
+			arr.push(key + '=' + ob[key]);
+		}
+	}
+
+	return arr.toString();
+}
+
+var orm = {
+	// to grab all from table
+	selectAll: function(tableInput, cb) {
+		var queryString = 'SELECT * FROM ' + tableInput + ';';
+		connection.query(queryString, function(err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+	},
+	//to insert burger into table
+	insertOne: function(table, cols, vals, cb) {
+		var queryString = 'INSERT INTO ' + table;
+
+		queryString += ' (';
+		queryString += cols.toString();
+		queryString += ') ';
+		queryString += 'VALUES (';
+		queryString += printQuestionMarks(vals.length);
+		queryString += ') ';
+
+		console.log(queryString);
+		console.log(vals);
+
+		connection.query(queryString, vals, function(err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+	},
+
+	// to update burger
+	updateOne: function(table, objColVals, condition, cb) {
+		var queryString = 'UPDATE ' + table;
+
+		queryString += ' SET ';
+		queryString += objToSql(objColVals);
+		queryString += ' WHERE ';
+		queryString += condition;
+
+		console.log(queryString);
+
+		connection.query(queryString, function(err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+	}
+};
+
+// export 
+module.exports = orm;
--- burger/db/seeds.sql
+++ burger/db/seeds.sql
@@ -1,6 +1,7 @@
-USE "burgers_db";
+-- work on the burgers_db
+USE `burgers_db`;
 
 -- insert 3 burgers
-INSERT INTO "burgers_table" (burger_name, devoured, date) VALUES ('Hamburger', false, CURRENT_TIMESTAMP);
-INSERT INTO "burgers_table" (burger_name, devoured, date) VALUES ('BBQ', false, CURRENT_TIMESTAMP);
-INSERT INTO "burgers_table" (burger_name, devoured, date) VALUES ('Cheese', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Plain Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Cheese', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Salmon', false, CURRENT_TIMESTAMP);
